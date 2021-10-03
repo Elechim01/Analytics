000215e5e4a6bc4723a3a150278dbd8a2da74b52
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-10-03 15:31:19 +0200

    Initial Commit

---
+++ Shared/AnalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 03/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
